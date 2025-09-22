5351092e45c8951c1bc3b97fc0e2edde6b6e46b4
Marco Christoforou <mchristoforou@akersystems.com>
2025-09-22 14:12:16 +0100

    Test UI app is ignored when change in API

--- ui/ui-api/src/app.controller.ts
+++ ui/ui-api/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// Test to see if CI/CD works
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
