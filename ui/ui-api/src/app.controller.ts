fc221829ea102e23c87359ea77e2e66a515a87f3
Marco Christoforou <mchristoforou@akersystems.com>
2025-09-23 10:47:17 +0100

    Test change ui-api

--- ui/ui-api/src/app.controller.ts
+++ ui/ui-api/src/app.controller.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service';
 
 // Test to see if CI/CD works
 // Another test
+// Another test 2
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
