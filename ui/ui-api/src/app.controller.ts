b1e6409bc3d9c4ee1c9fc537fee5a1bc9622b13c
Marco Christoforou <mchristoforou@akersystems.com>
2025-09-23 11:08:33 +0100

    Update file for test change

--- ui/ui-api/src/app.controller.ts
+++ ui/ui-api/src/app.controller.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 // Another test
 // Another test 2
 // Another test 3
+// Another test 4
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
