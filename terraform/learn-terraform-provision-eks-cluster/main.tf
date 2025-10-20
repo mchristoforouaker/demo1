684c7cbfbcb5ae332693223ce9c7352950e81c24
Marco Christoforou <mchristoforou@akersystems.com>
2025-10-20 13:35:02 +0100

    Set cluster name to fixed

--- terraform/learn-terraform-provision-eks-cluster/main.tf
+++ terraform/learn-terraform-provision-eks-cluster/main.tf
@@ -15,7 +15,8 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+#  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "demo1"
 }
 
 resource "random_string" "suffix" {
