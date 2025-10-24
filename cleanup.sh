55df879ffec696182fdcf152952bd2736fcac2db
Marco Christoforou <mchristoforou@akersystems.com>
2025-10-24 13:53:42 +0100

    Fix cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -3,5 +3,5 @@
 kubectl delete -k deployment/dev/ui-app
 kubectl delete -k deployment/dev/keyCloak
 
-cd ../terraform/learn-terrfrom-provision-eks-cluster
+cd ./terraform/learn-terraform-provision-eks-cluster
 terraform destroy -auto-approve
